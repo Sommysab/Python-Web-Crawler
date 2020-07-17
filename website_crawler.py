bcac4a45093e21bceaf53cdea9d3ad59e7c2dee1
sommysab <sommysabudeh@yahoo.com>
2020-07-18 00:39:58 +0100

    update

---
+++ website_crawler.py
@@ -0,0 +1,25 @@
+import requests, re, urlparse
+
+
+
+target_url = "https://path/to/web-address"
+target_links = []
+
+def extract_links_from(url):
+    response = requests.get(target_url)
+    return re.findall('(?:href=")(.*?)"', response.content)
+
+def crawl(url):
+    href_links = extract_links_from(url)
+    # print(href_links)
+    for link in href_links:
+        link = urlparse.urljoin(url, link)
+
+        if "#" in link: 
+            link = link.split("#")[0]
+
+        if target_url in link and link not in target_links:
+            target_links.append(link)
+            crawl(link)
+
+crawl(target_url)
